4d6a6df72ba5e695e2ce3c2d270b4d8ead8eb7eb
alberto venosa <alberto.harres@gmail.com>
2020-03-11 17:07:44 -0300

    make it better

--- index.js
+++ index.js
@@ -3,6 +3,52 @@ $(document).ready(function () {
     $('#cover').hide()
     $('#form').show()
   })
+
+  function validarCPF(cpf) {	
+    cpf = cpf.replace(/[^\d]+/g,'');	
+    if(cpf == '') return false;	
+    // Elimina CPFs invalidos conhecidos	
+    if (cpf.length != 11 || 
+      cpf == "00000000000" || 
+      cpf == "11111111111" || 
+      cpf == "22222222222" || 
+      cpf == "33333333333" || 
+      cpf == "44444444444" || 
+      cpf == "55555555555" || 
+      cpf == "66666666666" || 
+      cpf == "77777777777" || 
+      cpf == "88888888888" || 
+      cpf == "99999999999")
+        return false;		
+    // Valida 1o digito	
+    add = 0;	
+    for (i=0; i < 9; i ++)		
+      add += parseInt(cpf.charAt(i)) * (10 - i);	
+      rev = 11 - (add % 11);	
+      if (rev == 10 || rev == 11)		
+        rev = 0;	
+      if (rev != parseInt(cpf.charAt(9)))		
+        return false;		
+    // Valida 2o digito	
+    add = 0;	
+    for (i = 0; i < 10; i ++)		
+      add += parseInt(cpf.charAt(i)) * (11 - i);	
+    rev = 11 - (add % 11);	
+    if (rev == 10 || rev == 11)	
+      rev = 0;	
+    if (rev != parseInt(cpf.charAt(10)))
+      return false;		
+    return true;   
+  }
+
+  function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  $('#cpf').focusout(function () {
+    console.log('cpf', $(this).val())
+  })
   
   $("#cep").focusout(function(){
 		//Início do Comando AJAX
@@ -29,6 +75,27 @@ $(document).ready(function () {
 				//melhorando a experiência do usuário
 				$("#numero").focus();
 			}
-		});
-	});
+		})
+  })
+
+  function checkAllFields () {
+    
+  }
+  
+  $('#submit').click(function () {
+    let cpf = $('#cpf').val()
+    let nome_completo = $('#nome_completo').val()
+    let nome_corretora = $('#nome_corretora').val()
+    let regional = $('#regional').val()
+    let email = $('#email').val()
+    let telefone = $('#telefone').val()
+    let cep = $('#cep').val()
+    let logradouro = $('#logradouro').val()
+    let numero = $('#numero').val()
+    let complemento = $('#complemento').val()
+    let bairro = $('#bairro').val()
+    let uf = $('#uf').val()
+    let voltagem = $("div[name=voltagem]").val()
+    let brinde = $("div[name=brinde]").val()
+  })
 })
